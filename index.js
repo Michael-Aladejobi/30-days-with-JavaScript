f8ef374c745268418a9c0ecca5a5871f45a68403
mayorgt_ <mikkystechs@gmail.com>
2024-07-09 14:04:53 +0100

    onclick events

--- index.js
+++ index.js
@@ -1,5 +1,17 @@
 // document.getElementById("count-el").innerText = 5;
 
-let count = 5
-count += 1
-console.log(count)
+let bonusPoints = 50;
+console.log(bonusPoints);
+
+bonusPoints = bonusPoints + 50;
+console.log(bonusPoints);
+
+bonusPoints = bonusPoints - 125;
+console.log(bonusPoints);
+
+bonusPoints = bonusPoints + 45;
+console.log(bonusPoints);
+
+function increment() {
+    console.log("The button was clicked!");
+}

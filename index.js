a286029bf26dab5df6eac95300a3de3cd5110502
mayorgt_ <mikkystechs@gmail.com>
2024-07-09 14:29:33 +0100

    playing with functions

--- index.js
+++ index.js
@@ -1,17 +1,29 @@
 // document.getElementById("count-el").innerText = 5;
 
-let bonusPoints = 50;
-console.log(bonusPoints);
+// let bonusPoints = 50;
+// console.log(bonusPoints);
 
-bonusPoints = bonusPoints + 50;
-console.log(bonusPoints);
+// bonusPoints = bonusPoints + 50;
+// console.log(bonusPoints);
 
-bonusPoints = bonusPoints - 125;
-console.log(bonusPoints);
+// bonusPoints = bonusPoints - 125;
+// console.log(bonusPoints);
 
-bonusPoints = bonusPoints + 45;
-console.log(bonusPoints);
+// bonusPoints = bonusPoints + 45;
+// console.log(bonusPoints);
 
-function increment() {
-    console.log("The button was clicked!");
-}
+// function increment() {
+//     console.log("The button was clicked!");
+// }
+
+// let lap1 = 34;
+// let lap2 = 33;
+// let lap3 = 36;
+
+// function lapSum() {
+//     console.log(lap1 + lap2 + lap3);
+// }
+
+// lapSum()
+
+let lapsCompleted = 0

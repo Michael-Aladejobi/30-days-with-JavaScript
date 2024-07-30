aaec1ab8caff00d6d1bc97c5eda6a92fe19dcc3c
Michael-Aladejobi <mikkystechs@gmail.com>
2024-07-30 13:43:38 +0100

    ...

--- index.js
+++ index.js
@@ -1,29 +1,7 @@
-// document.getElementById("count-el").innerText = 5;
+let count = 0;
+const countEl = document.getElementById("count-el");
+const incrementBtn = document.getElementById("increment-btn");
 
-// let bonusPoints = 50;
-// console.log(bonusPoints);
-
-// bonusPoints = bonusPoints + 50;
-// console.log(bonusPoints);
-
-// bonusPoints = bonusPoints - 125;
-// console.log(bonusPoints);
-
-// bonusPoints = bonusPoints + 45;
-// console.log(bonusPoints);
-
-// function increment() {
-//     console.log("The button was clicked!");
-// }
-
-// let lap1 = 34;
-// let lap2 = 33;
-// let lap3 = 36;
-
-// function lapSum() {
-//     console.log(lap1 + lap2 + lap3);
-// }
-
-// lapSum()
-
-let lapsCompleted = 0
+function increment() {
+    countEl.textContent = count ++;
+}

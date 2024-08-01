bc15526bc32895fb60e6b8dc2244c0d5aa6f1f2e
Michael-Aladejobi <mikkystechs@gmail.com>
2024-08-01 09:12:48 +0100

    ...

--- index.js
+++ index.js
@@ -1,7 +1,32 @@
-let count = 0;
-const countEl = document.getElementById("count-el");
-const incrementBtn = document.getElementById("increment-btn");
+const personsArray = [
+    {
+        name: "person1",
+        age: 30,
+        country: "USA",
+    },
+    {
+        name: "person2",
+        age: 36,
+        country: "RUSSIA",
+    },
+    {
+        name: "person3",
+        age: 38,
+        country: "INDIA",
+    },
+    {
+        name: "person3",
+        age: 38,
+        country: "INDIA",
+    },
+    {
+        name: "person4",
+        age: 40,
+        country: "INDIA",
+    },
+];
 
-function increment() {
-    countEl.textContent = count ++;
-}
+let checkEveryArrayMethodWithExample = personsArray.every((person) => {
+    return person.age >= 30;
+});
+console.log(checkEveryArrayMethodWithExample);

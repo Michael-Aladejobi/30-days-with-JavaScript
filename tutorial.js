fd155b6f37b21c2941d930baef5d5121e25f94ed
Michael-Aladejobi <mikkystechs@gmail.com>
2025-06-14 09:04:38 +0100

    ...

--- tutorial.js
+++ tutorial.js
@@ -1,1 +1,14 @@
-console.log("hello world")
+const prompt = require("prompt-sync")();
+const name = prompt("What is your name? ");
+console.log("Hello", name, "Welcome to our game!");
+
+const shouldWePlay = prompt("Do you want to play? ")
+
+const condition  = shouldWePlay.toLowerCase() === "yes"
+console.log(condition)
+
+if(condition){
+    console.log("We will play!")
+}else{
+    console.log(" Okay :(")
+}

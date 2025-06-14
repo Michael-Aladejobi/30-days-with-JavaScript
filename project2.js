f73acc9d5c67d5f37b5000c9124e46909e5318e3
Michael-Aladejobi <mikkystechs@gmail.com>
2025-06-14 10:46:00 +0100

    ...

--- project2.js
+++ project2.js
@@ -1,26 +1,66 @@
 const prompt = require("prompt-sync")();
 
-const number1 = parseFloat(prompt("Enter Number 1: "));
-const number2 = parseFloat(prompt("Enter Number 2: "));
-const operator = prompt("Enter Sign: ");
 
-let result;
-switch (operator) {
-  case "+":
-    result = number1 + number2;
-    break;
-  case "-":
-    result = number1 - number2;
-    break;
-  case "/":
-    result = number1 / number2;
-    break;
-  case "*":
-    result = number1 * number2;
-    break;
-  default:
-    console.log("Invalid Input");
-    break;
+function getNumber(numberString) {
+  while (true) {
+    const number = parseFloat(prompt("Enter number " + numberString + ": "));
+
+    if (isNaN(number)) {
+      console.log("Invalid Input");
+    } else {
+      return number
+    }
+  }
 }
 
-console.log(number1, operator, number2, "=", result);
+const number1 = getNumber("One")
+const number2 = getNumber("Two")
+
+// while (true) {
+//   number1 = parseFloat(prompt("Enter Number 1: "));
+
+//   if (isNaN(number1)) {
+//     console.log("Invalid Input!");
+//   } else {
+//     break;
+//   }
+// }
+
+// while(true){
+//     number2 = parseFloat(prompt("Enter Number 2: "));
+//     if(isNaN(number2)){
+//         console.log("Invald Input!")
+//     }else{
+//         break
+//     }
+
+// }
+// const operator = prompt("Enter Sign: ");
+
+// let result;
+// switch (operator) {
+//   case "+":
+//     result = number1 + number2;
+//     break;
+//   case "-":
+//     result = number1 - number2;
+//     break;
+//   case "/":
+//     result = number1 / number2;
+//     break;
+//   case "*":
+//     result = number1 * number2;
+//     break;
+//   default:
+//     console.log("Invalid Input");
+//     break;
+// }
+
+// console.log(number1, operator, number2, "=", result);
+
+// // let i = 0;
+// // while (i < 10) {
+// //   i++;
+// //   if (i === 5) continue;
+// //   console.log(i);
+// // }
